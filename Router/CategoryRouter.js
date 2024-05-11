8aff59b4c29adebea2220ff6a3db586f3f75557f
mannumishra <mannumishra2207@outlook.com>
2024-05-11 12:39:44 +0530

    add change

--- Router/CategoryRouter.js
+++ Router/CategoryRouter.js
@@ -1,12 +1,12 @@
 const { createCategory, getCategory, getSingleCategory, deleteCategory, updateCategory } = require("../Controllar/CategoryControllar")
-const { verifyAdmin } = require("../Utils/Verifytoken")
+// const { verifyAdmin } = require("../Utils/Verifytoken")
 
 const categoryRouter = require("express").Router()
 
-categoryRouter.post("/category",verifyAdmin , createCategory)
+categoryRouter.post("/category" , createCategory)
 categoryRouter.get("/category", getCategory)
 categoryRouter.get("/category/:_id" , getSingleCategory)
-categoryRouter.delete("/category/:_id",verifyAdmin , deleteCategory)
-categoryRouter.put("/category/:_id" ,verifyAdmin, updateCategory)
+categoryRouter.delete("/category/:_id", deleteCategory)
+categoryRouter.put("/category/:_id" , updateCategory)
 
 module.exports = categoryRouter
